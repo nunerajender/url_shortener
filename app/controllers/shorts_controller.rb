81ca0ca9a59033a7df29b4c807f35bfaf01857a8
nunerajender <rajender0526@gmail.com>
2015-03-24 16:38:12 +0530

    type your commit message here

---
+++ app/controllers/shorts_controller.rb
@@ -0,0 +1,31 @@
+class ShortsController < ApplicationController
+
+  def show
+    @short = Short.find(params[:id].to_i(36))
+
+    respond_to do |format|
+      format.html { redirect_to @short.long }
+    end
+  end
+
+	def new
+		@short = Short.new
+
+		respond_to do |format|
+			format.html 
+		end
+	end
+
+  
+  def create
+    @short = Short.new(params[:short])
+
+    respond_to do |format|
+      if @short.save
+        format.html { render action: "show" } 
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = '734d8c0f9c264578deeec6ae585443e271d0dc15efe9942bf022b3ac58a15b855a22869ba2d8114e394e04ea4e1f6c7ef068fe5823567d12319f3cffdfdc3260'
